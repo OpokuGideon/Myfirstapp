e11d41788be74befc5d725fb37bff50531ef368f
OpokuGideon <gideonopokun@gmail.com>
2023-04-13 10:34:33 -0700

    Section 5

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,14 +1,14 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SQLitePCL;
 
 namespace API.Controllers
-{
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+{   
+    [Authorize]
+    public class UsersController : BaseApiController
     {
     
         private readonly DataContext _context;
@@ -18,7 +18,7 @@ namespace API.Controllers
             _context = context;
         
         }   
-
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Net.Http.Headers;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string Username {get; set;}
+
+       [Required]
+        public string Password { get; set; }
+    }
+}
